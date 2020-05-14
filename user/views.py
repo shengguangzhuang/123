f5c573c7eb3658b7d2b94acd24488bc6da892d4f
sgz2 <sgz2@163.com>
2020-05-14 16:19:35 +0800

    定义函数视图

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+#以下代码用于定义函数视图
+
+
+# def register('请求对象'):
+#     return '响应对象'
+
+from django import http
+
+def register(request):
+    return http.HttpResponse('假装这是个注册页面')
